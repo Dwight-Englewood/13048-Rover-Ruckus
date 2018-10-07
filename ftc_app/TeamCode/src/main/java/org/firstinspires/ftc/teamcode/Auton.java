b2445b8c94d98d014ec4904b9da7aac214246008
TKANE1 <tanaykane@Aparnas-MacBook-Air.local>
2018-10-07 08:33:45 -0400

    added Auton

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton.java
@@ -3,26 +3,32 @@ package org.firstinspires.ftc.teamcode;
 /**
  * Created by Kevin on 10/05/18.
  */
-    import com.qualcomm.hardware.bosch.BNO055IMU;
-        import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cColorSensor;
-        import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
-        import com.qualcomm.robotcore.util.Range;
-        import com.qualcomm.robotcore.hardware.*;
-        import org.firstinspires.ftc.robotcore.external.Telemetry;
-        import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-        import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-        import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
-        import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
-        import java.util.*;
-    import org.firstinspires.ftc.teamcode.MovementEnum;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-    import org.firstinspires.ftc.teamcode.bot;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.bot;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cColorSensor;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.*;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+import org.firstinspires.ftc.teamcode.bot;
 
 @com.qualcomm.robotcore.eventloop.opmode.Autonomous(name="autonomous",group="Autonomous")
 public class Auton {
+    bot robot = new bot();
+    private ElapsedTime runtime = new ElapsedTime();
     @Override
     public void init() {
         robot.init(hardwareMap, telemetry, false);
+//      robot.resetServo();
         telemetry.addData("Status", "Initialized");
     }
 
@@ -33,7 +39,9 @@ public class Auton {
 
     @Override
     public void loop() {
-
+        if(runtime.milliseconds() >= 2000){
+            robot.hook.setPower(-0.5);
+        }
     }
 
     @Override
